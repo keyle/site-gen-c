7abe699550dc7499482b746bf83064b105982412
keyle <keyle@capsule.org>
2023-12-31 17:27:02 +1000

    bug fix

--- src/files.c
+++ src/files.c
@@ -14,20 +14,22 @@ file_t read_file_content(const char file_name[static 1]) {
     size_t file_size = ftell(fp);
     rewind(fp);
 
-    char* content = (char*)malloc(file_size);
+    char* content = (char*)malloc(file_size + 1);
     if (!content) {
         fprintf(stderr, "Failed to allocate memory in read_file_content()\n");
         fclose(fp);
         return (file_t){.error = true};
     }
 
-    size_t bytes_read = fread(content, 1, file_size, fp);
+    size_t bytes_read = fread(content, sizeof(char), file_size, fp);
     if (bytes_read != file_size) {
         fprintf(stderr, "Failed to read the entire file in read_file_content()\n");
         free(content);
         fclose(fp);
         return (file_t){.error = true};
     }
+
+    content[file_size] = '\0'; // bug fix, sometimes we would keep going!
 
     fclose(fp);
     return (file_t){.data = content, .size = file_size, .error = false};
