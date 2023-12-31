c40bba8f44310e67425160ee372254aef2645823
keyle <keyle@capsule.org>
2023-12-31 17:33:50 +1000

    slightly better packing

--- src/article.h
+++ src/article.h
@@ -5,16 +5,16 @@
 #include <stdio.h>
 
 typedef struct {
-    char* path;
-    char* file;
     char* markdown;
     char* html;
     char* title;
-    bool is_blog;
-    char* pub_date;
+    char* path;
+    char* file;
     char* url;
     char* description;
     char* tags;
+    char* pub_date;
+    bool is_blog;
 } Article;
 
 void sort_articles_date_descending(Article** article_list, size_t article_count);
